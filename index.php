580f9680441d1dda80015a74ffd5676767d54486
sachin121991 <41074811+sachin121991@users.noreply.github.com>
2020-01-29 04:58:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 
-
 <!DOCTYPE html>
 <html>
-<head>
-<title>Hello, World! Page</title>
-</head>
 <body>
-<?php
-	echo 'Hello, World!';
-?>
+
+
+<h2 style="background-color:Orange;font-size:50px;color:red;">Latest Flying Beast video</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/SksubNZ2brk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 </body>
 </html>
 
